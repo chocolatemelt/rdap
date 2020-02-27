527ead11921b80e9c6c1cbf927cd7b45dedcf7db
Kevin Zhang <kzhang@hubspot.com>
2020-02-27 11:43:34 -0500

    simplify lib

--- lake/rgapi.py
+++ lake/rgapi.py
@@ -11,11 +11,7 @@ RGAPI_KEY = config["riot"]["api_key"]
 RGAPI_REGION = config["riot"]["region"]
 
 
-def get_route_to(route):
-    return f"https://{RGAPI_REGION}.api.riotgames.com{route}"
-
-
-def get_auth_headers():
+def auth_headers():
     return {
         "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
         "X-Riot-Token": RGAPI_KEY,
@@ -23,16 +19,25 @@ def get_auth_headers():
     }
 
 
+def get(route):
+    return requests.get(
+        f"https://{RGAPI_REGION}.api.riotgames.com{route}", headers=auth_headers()
+    )
+
+
 def get_eaid(summoner_name):
-    ROUTE = f"/lol/summoner/v4/summoners/by-name/{urllib.parse.quote(summoner_name)}"
-    response = requests.get(get_route_to(ROUTE), headers=get_auth_headers())
-    return response.json()["accountId"]
+    res = get(f"/lol/summoner/v4/summoners/by-name/{urllib.parse.quote(summoner_name)}")
+    return res.json()["accountId"]
 
 
 def get_matchlist(eaid):
-    ROUTE = f"/lol/match/v4/matchlists/by-account/{eaid}"
-    response = requests.get(get_route_to(ROUTE), headers=get_auth_headers())
-    return response.json()["matches"]
+    res = get(f"/lol/match/v4/matchlists/by-account/{eaid}")
+    return res.json()["matches"]
+
+
+def get_match(match_id):
+    res = get(f"/lol/match/v4/matches/{match_id}")
+    return res.json()
 
 
 if __name__ == "__main__":
@@ -42,5 +47,7 @@ if __name__ == "__main__":
         sys.exit(1)
     print(f"getting data for {SUMMONER}...")
     print(f"{SUMMONER}'s encrypted account ID is {get_eaid(SUMMONER)}")
-    print(json.dumps(get_matchlist(get_eaid(SUMMONER)), indent=2))
+    print(
+        json.dumps(get_match(get_matchlist(get_eaid(SUMMONER))[0]["gameId"]), indent=2)
+    )
 
